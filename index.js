747fbc9c37e53ee1df81965d2b62e4b3165d5b30
Josh Bowen <bowenjosh@google.com>
2017-05-04 11:33:54 -0600

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World v5\n");
+  response.end("Hello Hook Test v6\n");
 });
 
 server.listen(9000);
